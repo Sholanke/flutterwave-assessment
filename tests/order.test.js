010c7c16b26e6a0a6baf48963716488f2cab3da2
Olamide Sholanke <olamide@Olamides-MacBook-Pro.local>
2021-01-31 16:28:19 +0100

    test : sum up

--- tests/order.test.js
+++ tests/order.test.js
@@ -1,5 +1,6 @@
-import { DECREMENTER, INCREMENTER } from "../utils/Order";
+import { DECREMENTER, INCREMENTER, sumUp, ticketTotalPrice } from "../utils/Order";
 
+// incrementer tests
 test("Should increment number ", () => {
   const safeNumber = 0;
   const safeNumberPlusOne = INCREMENTER(safeNumber);
@@ -12,6 +13,7 @@ test("Should not increment past max number ", () => {
   expect(safeNumberPlusOne).toBe(safeNumber);
 });
 
+// decrementer tests
 test("Should decrement number ", () => {
   const safeNumber = 2;
   const safeNumberPlusOne = DECREMENTER(safeNumber);
@@ -22,4 +24,23 @@ test("Should not decrement past 0 ", () => {
   const safeNumber = 0;
   const safeNumberPlusOne = DECREMENTER(safeNumber);
   expect(safeNumberPlusOne).toBe(safeNumber);
+});
+
+//tickets total price
+test("Should get ticket total price", () => {
+  const safeTicket = {
+    numberOfTickets: 1,
+    perTicketPrice: 100
+  };
+  const safeTicketTotalPrice = ticketTotalPrice(safeTicket);
+  expect(safeTicketTotalPrice).toBe(
+    safeTicket.numberOfTickets * safeTicket.perTicketPrice
+  );
+});
+
+//sum up array of numbers
+test("Should sum up array of numbers", () => {
+  const safeArrayOfNumber = [1, 2, 3];
+  const safeSumUp = sumUp(safeArrayOfNumber);
+  expect(safeSumUp).toBe(6);
 });
